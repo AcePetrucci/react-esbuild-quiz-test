4fec29e6edea3a608a998fd8c3c69ec8d15fc014
André Ávila Petrucci <ace.petrucci@outlook.com>
2021-08-17 05:20:26 -0300

    set up react esbuild typescript boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HomeComponent } from './pages/Home';
+
+/**
+ * Render the React Bootstrap
+ */
+
+ReactDOM.render(<HomeComponent />, document.getElementById('root'));
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,3 @@
+import React, { memo } from 'react';
+
+export const HomeComponent = (_props) => <div> Home Page! </div>;
